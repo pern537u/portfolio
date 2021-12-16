c3d0da806c2a7c1abf17eea09a89baa1bdd6b13b
Pernille Nørregaard <pern537u@edu.zealand.dk>
2021-12-16 09:07:48 +0100

    Index og arbejde filer færdig til alle formater + læs mere knapper

---
+++ script.js
@@ -0,0 +1,36 @@
+function myFunction() {
+    var mereText = document.getElementById("mere");
+    var læsMerePil = document.getElementById("læsmerepil")
+  
+    if (læsMerePil.style.transform === "rotate(-180deg)"){
+      læsMerePil.style.transform = "rotate(0)"; 
+      mereText.style.display = "none";
+    } else {
+        læsMerePil.style.transform = "rotate(-180deg)"; 
+        mereText.style.display = "inline";
+    }
+  }
+  function myFunctionJive() {
+    var mereText = document.getElementById("merejive");
+    var læsMerePil = document.getElementById("læsmerepiljive")
+  
+    if (læsMerePil.style.transform === "rotate(-180deg)"){
+      læsMerePil.style.transform = "rotate(0)"; 
+      mereText.style.display = "none";
+    } else {
+        læsMerePil.style.transform = "rotate(-180deg)"; 
+        mereText.style.display = "inline";
+    }
+  }
+  function myFunctionBb() {
+    var mereText = document.getElementById("merebb");
+    var læsMerePil = document.getElementById("læsmerepilbb")
+  
+    if (læsMerePil.style.transform === "rotate(-180deg)"){
+      læsMerePil.style.transform = "rotate(0)"; 
+      mereText.style.display = "none";
+    } else {
+        læsMerePil.style.transform = "rotate(-180deg)"; 
+        mereText.style.display = "inline";
+    }
+  }
